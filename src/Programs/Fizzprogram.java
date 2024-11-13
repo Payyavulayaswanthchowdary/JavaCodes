3c6a9b54626b380d1b48b43dda45a7b799625d7f
yaswanth P <yaswanthpayyavula77@gmail.com>
2024-11-13 21:36:43 +0530

    FizzProgram

---
+++ src/Programs/Fizzprogram.java
@@ -0,0 +1,24 @@
+package Programs;
+
+import java.util.Scanner;
+
+public class Fizzprogram {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        int num = scr.nextInt();
+        for(int i = 1;i <= num;i++){
+            if(i % 3 == 0 && i % 5 == 0 ){
+                System.out.print("\"FizzBuzz\",");
+            }
+            else if(i % 3 == 0){
+                System.out.print("\"Fizz\",");
+            }
+            else if(i % 5 == 0){
+                System.out.print("\"Buzz\",");
+            }
+            else{
+                System.out.print(i+",");
+            }
+ }
+}
+}
